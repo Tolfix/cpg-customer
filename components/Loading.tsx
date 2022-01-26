0b5e73123c65a1e3c992dd267ec4904850eabd64
Kai <21qtkai@gmail.com>
2022-01-26 15:12:50 +0000

    Fixed my mistakes, and updated ESLint.

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,17 +1,11 @@
-
-export default () =>
-{
-    return (
-        <>
-            <div className="CenterScreen">
-                <div className="w-full max-w-xs">
-                    <div className="text-center">
-                        <div className="LoadText">
-                            Loading..
-                        </div>
-                    </div>
+export default () => (
+    <div className="CenterScreen">
+        <div className="w-full max-w-xs">
+            <div className="text-center">
+                <div className="LoadText">
+                    Loading..
                 </div>
             </div>
-        </>
-    )
-}
+        </div>
+    </div>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,49 @@
 import { ICustomer } from '@cpg/Interfaces/Customer.interface';
+import type { NextPage } from 'next'
 import { useSession } from 'next-auth/react';
-import {Component, useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import Loading from "../components/Loading";
 
-class Home extends Component
-{
-    render()
-    {
-        const session = useSession();
-        // @ts-ignore
-        let token = session.data?.user.email
-        const [customer, setCustomer] = useState<ICustomer | null>(null);
+const Home: NextPage = () => {
+    const session = useSession();
+    // @ts-ignore
+    let token = session.data?.user.email
+    const [customer, setCustomer] = useState<ICustomer | null>(null);
 
-        useEffect(() =>
-        {
-            fetch(`${process.env.NEXT_PUBLIC_CPG_DOMAIN}/v2/customers/my/profile`,
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            }).then(res => res.json()).then(data => {
-                setCustomer(data);
-            });
-        }, []);
+    useEffect(() => {
+        fetch(`${process.env.NEXT_PUBLIC_CPG_DOMAIN}/v2/customers/my/profile`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(res => res.json()).then(data => {
+            setCustomer(data);
+        });
+    }, []);
 
-        if (!customer)
-            return (
-                <>
-                    <Loading/>
-                </>
-            )
-
+    if(!customer)
         return (
             <>
+                <Loading/>
+            </>
+        )
 
-                {/* Customer portal, with tailwind */}
-                <div className="flex flex-col justify-center items-center mt-32 w-screen">
-                    <div className="max-w-xs">
-                        <div className="text-center">
-                            <div className="text-gray-700 text-xl font-bold">
-                                Welcome {customer.personal.first_name} {customer.personal.last_name}
-                            </div>
+    return (
+        <>
+
+            {/* Customer portal, with tailwind */}
+            <div className="flex flex-col justify-center items-center mt-32 w-screen">
+                <div className="max-w-xs">
+                    <div className="text-center">
+                        <div className="text-gray-700 text-xl font-bold">
+                            Welcome {customer.personal.first_name} {customer.personal.last_name}
                         </div>
                     </div>
                 </div>
-            </>
-        );
-    }
+            </div>
+        </>
+    );
 }
 
-export default Home;
+export default Home
