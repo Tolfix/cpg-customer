d2317ad32a3c7b8147cc87a982cb39937040b7b8
Tolfx <matteus@tolfx.dev>
2022-01-27 09:55:12 +0100

    Added new pages `orders/transactions`

---
+++ pages/orders/index.tsx
@@ -0,0 +1,35 @@
+import { IOrder } from "@cpg/Interfaces/Orders.interface"
+import { getSession } from "next-auth/react";
+
+export default (
+    {
+        orders
+    }: {
+        orders: IOrder
+    }
+) =>
+{
+
+}
+
+export async function getServerSideProps(context: any)
+{
+    const session = await getSession(context);
+    // @ts-ignore
+    let token = session?.user.email
+
+    const orders = await fetch(`${process.env.NEXT_PUBLIC_CPG_DOMAIN}/v2/customers/my/orders`,
+    {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    }).then(res => res.json());
+
+    return {
+        props: {
+            orders
+        }
+    }
+}
---
+++ pages/transactions/index.tsx
@@ -0,0 +1,33 @@
+import { ITransactions } from "@cpg/Interfaces/Transactions.interface"
+import { getSession } from "next-auth/react";
+
+export default ({
+    transactions
+}: {
+    transactions: ITransactions[]
+}) =>
+{
+
+}
+
+export async function getServerSideProps(context: any)
+{
+    const session = await getSession(context);
+    // @ts-ignore
+    let token = session?.user.email
+
+    const transactions = await fetch(`${process.env.NEXT_PUBLIC_CPG_DOMAIN}/v2/customers/my/transactions`,
+    {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    }).then(res => res.json());
+
+    return {
+        props: {
+            transactions
+        }
+    }
+}
