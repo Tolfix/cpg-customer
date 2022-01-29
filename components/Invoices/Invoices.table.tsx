7780e6394494adc3c40ba32272b3f91a25294a1c
Tolfx <matteus@tolfx.dev>
2022-01-29 15:46:23 +0100

    Better tables

--- components/Invoices/Invoices.table.tsx
+++ components/Invoices/Invoices.table.tsx
@@ -117,7 +117,7 @@ const InvoiceTable = ({
         const option = form.searchOption.value;
         const search = form.search.value;
 
-        if(option === "ocr")
+        if(option === "dates.invoice_date&id")
         {
             // parse the search, and get the invoice id and date
             // YYMMDDID
@@ -130,6 +130,54 @@ const InvoiceTable = ({
         return window.location.href = `/invoices?${option}=${search}`;
     }
 
+    const rowData = [
+        {
+            id: 'id',
+            name: "Id",
+            sortable: true,
+            queryFormat: () =>
+            {
+                return "id";
+            },
+        },
+        {
+            id: "ocr",
+            name: "OCR",
+            sortable: true,
+            queryFormat: () =>
+            {
+                return "dates.invoice_date&id";
+            },
+        },
+        {
+            id: "due_date",
+            name: "Due Date",
+            sortable: false,
+            queryFormat: () =>
+            {
+                return "dates.invoice_date";
+            },
+        },
+        {
+            id: "amount",
+            name: "Amount",
+            sortable: true,
+            queryFormat: () =>
+            {
+                return "amount";
+            },
+        },
+        {
+            id: "paid",
+            name: "Paid",
+            sortable: true,
+            queryFormat: () =>
+            {
+                return "paid";
+            },
+        },
+    ]
+
     return (
         <div className="flex flex-col">
             {/* Search for different */}
@@ -137,15 +185,12 @@ const InvoiceTable = ({
                 <form onSubmit={search} action="">
                     {/* Select for different categories */}
                     <select className='rounded' name="searchOption" id="">
-                        <option value="id">
-                            Id
-                        </option>
-                        <option value="ocr">
-                            OCR
-                        </option>
+                        {rowData.map((row) =>
+                            <option key={row.id} value={row.queryFormat()} className='uppercase'>{row.name}</option>
+                        )}
                     </select>
                     {/* Input for what to search */}
-                    <input className='rounded bg-gray-300' type="text" name='search' />
+                    <input className='rounded bg-gray-200 focus:bg-gray-100' type="text" name='search' />
                     {/* Button to search */}
                     <button type='submit' className='bg-purple-300 hover:bg-purple-400 rounded px-2'>Search</button>
                 </form>
@@ -156,64 +201,38 @@ const InvoiceTable = ({
                         <table className="min-w-full">
                             <thead className="bg-gray-50 dark:bg-gray-700">
                             <tr>
-                                <th scope="col"
-                                    className={`py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400 
-                                    ${isSelected('id') === "ascending" ? `bg-gray-200` : ``}`}>
-                                    <button
-                                        type="button"
-                                        onClick={() => requestSort('id')}
+                                {rowData.map(({id, name, sortable}) => (
+                                    <th
+                                        key={id}
+                                        className={`${sortable ? 'cursor-pointer' : ''} 
+                                        border-b border-gray-200 dark:border-gray-600 text-left 
+                                        py-4 px-6 font-medium text-xs 
+                                        text-gray-500 uppercase tracking-wider
+                                        ${isSelected(id) === "ascending" ? `bg-gray-200` : ``}
+                                        `}
+                                        onClick={() => sortable && requestSort(id)}
                                     >
-                                        Id
-                                    </button>
-                                </th>
-                                <th scope="col"
-                                    className="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
-                                    <button
-                                        type="button"
-                                    >
-                                        OCR
-                                    </button>
-                                </th>
-                                <th scope="col"
-                                    className={`py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400 
-                                    ${isSelected('dates.due_date') === "ascending" ? `bg-gray-200` : ``}`}>
-                                    <button
-                                        type="button"
-                                        onClick={() => requestSort('dates.due_date')}
-                                    >
-                                        Due Date
-                                    </button>
-                                </th>
-                                <th scope="col"
-                                    className={`py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400 
-                                     ${isSelected('amount') === "ascending" ? `bg-gray-200` : ``}`}>
-                                    <button
-                                        type="button"
-                                        onClick={() => requestSort('amount')}
-                                    >
-                                        Total
-                                    </button>
-                                </th>
-                                <th scope="col"
-                                    className={`py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400 
-                                    ${isSelected('paid') === "ascending" ? `bg-gray-200` : ``}`}>
-                                    <button
-                                        type="button"
-                                        onClick={() => requestSort('paid')}
-                                    >
-                                        Paid
-                                    </button>
-                                </th>
-
+                                        <button
+                                            type="button"
+                                            onClick={() => requestSort(id)}
+                                        >
+                                            {name}
+                                        </button>
+                                    </th>
+                                ))}
                                 <th scope="col" className={`relative py-3 px-6`}>
-                                    <span className="sr-only">More</span>
+                                    <span className="sr-only">
+                                        More
+                                    </span>
                                 </th>
                                 <th scope="col" className={`relative py-3 px-6`}>
-                                    <span className="sr-only">Preview</span>
+                                    <span className="sr-only">
+                                        Preview
+                                    </span>
                                 </th>
                             </tr>
                             </thead>
-                            <tbody className=''>
+                            <tbody>
                                 {items.map((invoice) => (
                                     <InvoiceData key={invoice.id} invoice={invoice}/>
                                 ))}
