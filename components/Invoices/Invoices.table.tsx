43bade14b8c6220caaee04d3089ecf6589b13f2e
Tolfx <matteus@tolfx.dev>
2022-01-29 21:25:14 +0100

    Moved sortable

--- components/Invoices/Invoices.table.tsx
+++ components/Invoices/Invoices.table.tsx
@@ -1,50 +1,8 @@
 import {IInvoice} from '@cpg/Interfaces/Invoice.interface';
 import { useSession } from 'next-auth/react';
 import React, {useState} from 'react';
+import useSortableData from '../Tables/Sortable';
 import InvoiceModal, { popupCenter } from './Invoice.modal';
-
-const useSortableData = (items: IInvoice[], config = null) =>
-{
-    const [sortConfig, setSortConfig] = React.useState<{
-        key: string;
-        direction: 'ascending' | 'descending';
-    } | null>(config);
-
-    const sortedItems = React.useMemo(() =>
-    {
-        const sortableItems = [...items];
-        if (sortConfig !== null)
-        {
-            sortableItems.sort((a, b) =>
-            {
-                // @ts-ignore
-                if (a[sortConfig.key] < b[sortConfig.key])
-                    return sortConfig.direction === 'ascending' ? -1 : 1;
-                // @ts-ignore
-                if (a[sortConfig.key] > b[sortConfig.key])
-                    return sortConfig.direction === 'ascending' ? 1 : -1;
-                return 0;
-            });
-        }
-        return sortableItems;
-    }, [items, sortConfig]);
-
-    // @ts-ignore
-    const requestSort = (key) =>
-    {
-        let direction = 'ascending';
-        if (
-            sortConfig &&
-            sortConfig.key === key &&
-            sortConfig.direction === 'ascending'
-        )
-            direction = 'descending';
-        // @ts-ignore
-        setSortConfig({key, direction});
-    };
-
-    return {items: sortedItems, requestSort, sortConfig};
-};
 
 const InvoiceData = ({invoice}: { invoice: IInvoice }) =>
 {
